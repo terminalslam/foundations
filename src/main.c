e779626b3e41bc007e03d6e742a10887f9512603
terminalslam <264676604+terminalslam@users.noreply.github.com>
2026-03-04 12:19:53 +0200

    ARCHIVE LOG 0003: Seek And You Shall Find

--- src/main.c
+++ src/main.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 
 int main(void) {
+
     int state = 1;
-    printf("SYSTEM STATUS: %d\n", state);
-    state = state -1 ;
-    printf("SYSTEM STATUS UPDATED: %d\n", state);
+    int *location = &state;
+
+    printf("SYSTEM: INITIATING POINTER.\n");
+
+    printf("MEMORY ADDRESS: %p\n", (void *)location);
+    printf("VALUE DRAWN FROM ADDRESS: %d\n", *location);
+
+    printf("SYSTEM: SEEK AND YOU SHALL FIND.\n");
+
     return 0;
 }
