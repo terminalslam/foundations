3b11fc208e1ea2ba0c7439ff791a3012f5139196
terminalslam <264676604+terminalslam@users.noreply.github.com>
2026-03-12 21:46:20 +0200

    ARCHIVE LOG 0004: Follow Me

--- src/main.c
+++ src/main.c
@@ -5,12 +5,13 @@ int main(void) {
     int state = 1;
     int *location = &state;
 
-    printf("SYSTEM: INITIATING POINTER.\n");
+    printf("SYSTEM: ACCESSING VALUE THROUGH ADDRESS.\n");
 
-    printf("MEMORY ADDRESS: %p\n", (void *)location);
-    printf("VALUE DRAWN FROM ADDRESS: %d\n", *location);
+    *location = 0;
 
-    printf("SYSTEM: SEEK AND YOU SHALL FIND.\n");
+    printf("UPDATED STATE: &d\n", state);
+    
+    printf("SYSTEM: FOLLOW HIM.");
 
     return 0;
 }
